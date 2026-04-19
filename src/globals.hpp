4fae19451a8dc5972dad41e0b0b1560cacabc356
glebshigin <gleb.shigin@flant.com>
2026-04-19 17:26:29 +0300

    remove old src

--- src/globals.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cstdint>
-
-constexpr uint64_t kScreenWidth{800};
-constexpr uint64_t kScreenHeight{600};
-
-constexpr uint64_t kScreenFps{60};
-constexpr uint64_t kNsPerFrame = 1'000'000'000 / kScreenFps;
